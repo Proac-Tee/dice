acbc61168e37fccf788eadeb7761e3ce23b90918
unknown <Taiwo Babatunde>
2022-05-24 12:28:10 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
+
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+if (randomImageSource2 > randomImageSource1) {
+  document.querySelector("h1").innerHTML = "🎈🎈Player 2 Wins!!!🎈🎈";
+
+} else if (randomImageSource1 > randomImageSource2) {
+  document.querySelector("h1").innerHTML = "🎈🎈Player 1 Wins!!!🎈🎈";
+
+} else {
+  document.querySelector("h1").innerHTML = "It's A tie !!!!"
+}
